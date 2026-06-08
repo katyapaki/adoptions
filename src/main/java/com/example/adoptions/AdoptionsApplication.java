c31cdeadcb75f944468a2d7fca4eeb34421845c2
Antonios Apostolakis <v-aapostolakis@eurobank.gr>
2026-06-08 11:33:27 +0300

    Fix compilation failures

---
+++ src/main/java/com/example/adoptions/AdoptionsApplication.java
@@ -0,0 +1,84 @@
+package com.example.adoptions;
+
+import io.modelcontextprotocol.client.McpClient;
+import io.modelcontextprotocol.client.McpSyncClient;
+import io.modelcontextprotocol.client.transport.HttpClientSseClientTransport;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.PromptChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.mcp.SyncMcpToolCallback;
+import org.springframework.ai.mcp.SyncMcpToolCallbackProvider;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+@SpringBootApplication
+public class AdoptionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdoptionsApplication.class, args);
+	}
+
+    @Bean
+    PromptChatMemoryAdvisor promptChatMemoryAdvisor(DataSource dataSource) {
+        var jdbc = JdbcChatMemoryRepository
+                .builder()
+                .dataSource(dataSource)
+                .build();
+
+        var chatMessageWindow = MessageWindowChatMemory
+                .builder()
+                .chatMemoryRepository(jdbc)
+                .build();
+
+        return PromptChatMemoryAdvisor
+                .builder(chatMessageWindow)
+                .build();
+    }
+
+}
+
+
+interface DogRepository extends ListCrudRepository<Dog, Integer> {
+}
+
+record Dog(@Id int id, String name, String owner, String description ){
+}
+
+
+
+@Controller
+@ResponseBody
+class AdoptionsController {
+
+    private final ChatClient ai;
+
+    AdoptionsController (ChatClient.Builder ai  ) {
+        this.ai = ai.build();
+    }
+
+    @GetMapping("/{user}/assistant")
+    String inquire(@PathVariable String user, @RequestParam String question) {
+        return ai
+                .prompt()
+                .user(question)
+                .call()
+                .content();
+    }
+}
